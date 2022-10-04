efc868c13fe76a0dfdff50486d7293fcf994e40b
Ian Finley <ianlukefinley@gmail.com>
2022-10-04 18:48:03 -0400

    Passing 56, db migrated

---
+++ back-end/src/db/migrations/202210041845532_reservationStatus.js
@@ -0,0 +1,10 @@
+exports.up = function (knex) {
+    return knex.schema.alterTable("reservations", (table) => {
+      table.string("status").defaultTo("booked").notNullable().index();
+    });
+  };
+  
+  exports.down = function (knex) {
+    return knex.schema.alterTable("reservations", (table) => {
+    });
+  };
